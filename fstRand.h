6c298ac8e1a6cfaafdec0adcf4ff90383a9053e4
Bobshnwinkle <jamesnwashburn@gmail.com>
2024-07-08 15:51:15 +0100

    first files

---
+++ fstRand.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+#include <vector>
+#include <iostream>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+class fstRand {
+
+public:
+	fstRand() {
+		seed = Next();
+	}
+	fstRand(float _seed) {
+		seed = _seed;
+	}
+
+	float Next() {
+		float out = distrobution((rdtsc() + seed), 3333 * ( 1.0f + (0.5 - seed)));
+		seed = out;
+		return out;
+	}
+private:
+	template <typename T>
+	static void printArray(vector<T> arr) {
+		for (size_t i = 0; i < arr.size(); i++) {
+			cout << i << ": " << arr[i] << endl;
+		}
+	}
+
+	float seed = 0.287124634;
+#ifdef _WIN32
+	static uint64_t rdtsc() {
+		auto val = __rdtsc();
+		return val % 10000;
+	}
+#else
+	static uint64_t rdtsc() {
+		unsigned int lo, hi;
+		__asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
+		auto val = ((uint64_t)hi << 32) | lo;
+		return val % 10000;
+}
+#endif
+
+	static float distrobution(float x, float mult = 3333) {
+		float comp = (x * mult) / 10000;
+		float out = comp - (truncf(comp));
+		return out;
+	}
+};
