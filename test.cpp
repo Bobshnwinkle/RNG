4566aa81368b9b41c0269e2f671f05d2bb8cc5d3
James Washburn <jamesnwashburn@gmail.com>
2026-09-15 17:25:02 +0100

    furhter colouring fixes

--- test.cpp
+++ test.cpp
@@ -201,7 +201,7 @@ class localTools{
                             if (x == Mean + borderFromLeft) text += "\e[0;34m";
                             else if (x == Median + borderFromLeft) text += "\e[0;32m";
                             else if (x == UQ + borderFromLeft) text += "\e[0;33m";
-                            else if (x == LQ + borderFromLeft) text += "\e[0;31m";
+                            else if (x == LQ + borderFromLeft) text += "\e[0;31ma";
                             text += GetBlock(F);
                             text += "\e[0m";
                         }
